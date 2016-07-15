d850207119dccffbde376ecf317bf1192d49a519
zuojianjun <zuojianjun@tuofeng.cn>
2016-07-15 15:36:17 +0800

    添加header

---
+++ share-ios/src/OpenShareHeader.h
@@ -0,0 +1,14 @@
+//
+//  OpenShareHeader.h
+//  openshare
+//
+//  Created by LiuLogan on 15/5/15.
+//  Copyright (c) 2015年 OpenShare <http://openshare.gfzj.us/>. All rights reserved.
+//
+
+#ifndef openshare_OpenShareHeader_h
+#define openshare_OpenShareHeader_h
+#import "OpenShare+QQ.h"
+#import "OpenShare+Weibo.h"
+#import "OpenShare+Weixin.h"
+#endif
